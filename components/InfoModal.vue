e26008edea7745e5cd04a23b0219d513e4d2bc34
David Emaye <emayeodavid@gmail.com>
2023-04-06 15:39:31 +0100

    first commit

---
+++ components/InfoModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <div>
+        <slot name="content" />
+        <button class="btn">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #2525252f;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+}
+.inner {
+  text-align: center;
+  width: 30%;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 20px;
+}
+
+.btn {
+  margin-top: 40px;
+  padding: 10px 20px;
+  background-color: rgb(37, 76, 236);
+  border: none;
+  color: white;
+  border-radius: 10px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main-container">
+    <div class="ctn">
+      <h1 class="header">Home Page</h1>
+      <InfoModal>
+        <template #content>
+          <div>
+            <h2>Contact Us</h2>
+            <h4>This is a contact form</h4>
+            <form action="">
+              <div class="input-ctn">
+                <label for="">Name</label>
+                <input type="text" />
+              </div>
+              <div class="input-ctn">
+                <label for="">Message</label>
+                <input type="text" />
+              </div>
+            </form>
+          </div>
+        </template>
+      </InfoModal>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style scoped>
+.header {
+  text-align: center;
+  margin-top: 20vh;
+}
+.input-ctn {
+  margin-top: 15px;
+}
+</style>
